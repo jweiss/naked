5c62741e4a4883d7333dfefc220c247f60d3b1a5
Jonathan Weiss <jw@innerewut.de>
2010-03-15 17:48:57 +0100

    namespace

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -3,7 +3,7 @@ begin
   DB_YML_PATH = release_path + "/config/database.yml"
   SCALARIUM_STATE_PATH = "/var/lib/scalarium/cluster_state.json"
 
-  json = File.read(SCALARIUM_STATE_PATH)
+  json = ::File.read(SCALARIUM_STATE_PATH)
   config = ActiveSupport::JSON.decode(json)
 
   ips_of_database_servers = config['roles']['rails-app']['instances'].map{|instance, instance_config| instance_config['private_dns_name']}
@@ -14,7 +14,7 @@ begin
   current_yml.update("production_master" => current_yml["production"].dup.update('host' => ips_of_database_servers.first))
   current_yml.update("production_salve" => current_yml["production"].dup.update('host' => ips_of_database_servers.last))
 
-  File.open(DB_YML_PATH, "w") do |db|
+  ::File.open(DB_YML_PATH, "w") do |db|
     db.print current_yml.to_yml
   end
 rescue Exception => e
